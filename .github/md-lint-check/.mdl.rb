6fadcefb56330edb063c28757532a591ec3772e9
Sergiy Poplavskyi <spopla@microsoft.com>
2020-09-29 07:54:48 +0300

    Added markdown check workflow (#33)
    * Added markdown check workflow
    
    - Links
    - Spell
    - Lint
    
    * Changed wordlist

---
+++ .github/md-lint-check/.mdl.rb
@@ -0,0 +1,8 @@
+# Enable all rules by default
+all
+
+# Extend line length, since each sentence should be on a separate line.
+rule 'MD013', :line_length => 99999
+
+# Nested lists should be indented with four spaces.
+rule 'MD007', :indent => 4
