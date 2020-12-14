9b01e1b68ec602875dd0fff9c793f481225543a4
mlani <mlani@gmail.com>
2020-12-14 16:56:58 +0100

    fixed storage account settings

--- build/terraform/infra/backend.tf
+++ build/terraform/infra/backend.tf
@@ -1,5 +1,7 @@
 terraform {
   backend "azurerm" {
+    resource_group_name   = "rg-GreenEnergyHub_Sandbox-S"
+    storage_account_name  = "enrgtfstateprod"
     container_name        = "tfstate"
     key                   = "terraform_infra.tfstate"
   }
