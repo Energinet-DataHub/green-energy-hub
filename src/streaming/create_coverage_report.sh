53cb242b3c5fd7a2d75cb731fb95f42277a1840b
BjarkeMeier <BjarkeMeier@users.noreply.github.com>
2021-01-21 11:33:50 +0100

    Test Python code coverage against threshold minimum requirements. (#636)
    * Test Python code coverage against threshold minimum requirements.
    
    * Threshold changed to desired 80% per review comment by @prtandrup.

--- src/streaming/create_coverage_report.sh
+++ src/streaming/create_coverage_report.sh
@@ -3,13 +3,14 @@
 # Script for generating HTML report of code coverage of the streaming
 # Python code base.
 #
-# Install coverage by executing:
+# Exit codes:
+#     On threshold success: 0
+#     On threshold failure: 1
+#
+# Requires the following modules that are installed in the Docker image
 #
 #     pip install coverage
-#
-# Not sure, but if necessary also install this module:
-#
-#     pip install pytest-cov
+#     pip install coverage-threshold
 # ====================================================================
 
 
@@ -17,12 +18,19 @@
 # This requires:
 # - An __init__.py file in tests/ folder
 # - The accompanying .coveragerc file
-coverage run -m pytest
+# - --branch option is required in order to generate required branch data for coverage-threshold
+coverage run --branch -m pytest tests/
 
-# Create HTML report
+# Create data for threshold evaluation
+coverage json
+
+# Create human reader friendly HTML report
 coverage html
 
-# Open report
-echo "Opening created HTML report."
-echo "TIP: This index.html can be viewed as HTML by adding VS Code extension like e.g. 'HTML Preview'."
-code htmlcov/index.html
+# Open HTML report
+#echo "Opening created HTML report."
+#echo "TIP: This index.html can be viewed as HTML by adding VS Code extension like e.g. 'HTML Preview'."
+#code htmlcov/index.html
+
+# Test if threshold requirements are met
+coverage-threshold
