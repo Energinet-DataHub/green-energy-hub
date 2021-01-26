371daf7f264269114fdd89db7373bce41d161ad8
Daniel Pethjen <daniel@pethjen.dk>
2021-01-26 13:55:11 +0100

    python coverage (#661)
    * CI updated
    
    * make sure steps are run
    
    * Threshold removed temporary
    
    * updated entrypoint
    
    * see what is in streaming folder
    
    * threshold added again
    
    * threshold lowered to 79
    
    * Reverted skip check back to origin
    
    Co-authored-by: Daniel <dph@energinet.dk>

--- .github/actions/databricks-unit-test/entrypoint.sh
+++ .github/actions/databricks-unit-test/entrypoint.sh
@@ -2,4 +2,11 @@
 
 cd ./src/streaming
 python setup.py install
-pytest tests
+# python coverage-threshold install
+pip install coverage-threshold
+coverage run --branch -m pytest tests/
+# Create data for threshold evaluation
+coverage json
+# Create human reader friendly HTML report
+coverage html
+coverage-threshold
